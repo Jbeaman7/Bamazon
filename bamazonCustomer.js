dffc7152650db0e263c460cd179a256017a4bc1d
Jordan Beaman <Jordanbeaman94@gmail.com>
2018-11-06 18:18:30 -0600

    Final changes

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -9,70 +9,69 @@ let connection = mysql.createConnection({
     database: "Bamazon"
 })
 
-connection.connect(function(error){
-    if(error)throw error;
+connection.connect(function (error) {
+    if (error) throw error;
     console.log("connected" + connection.threadId);
 });
 
-function startPrompt(){
+function startPrompt() {
     inquirer.prompt([
         {
             type: 'input',
-			name: 'item_id',
-			message: 'Please enter the Item ID of the item you want to purchase.'
-			
+            name: 'item_id',
+            message: 'Please enter the Item ID of the item you want to purchase.'
+
         },
         {
             type: 'input',
-			name: 'quantity',
-			message: 'How many would you like to purchase?'
+            name: 'quantity',
+            message: 'How many would you like to purchase?'
         }
 
-    ]).then(function(input){
-        console.log('Customer has selected: \n    item_id = '  + input.item_id + '\n    quantity = ' + input.quantity);
+    ]).then(function (input) {
+        console.log('Customer has selected: \n    item_id = ' + input.item_id + '\n    quantity = ' + input.quantity);
         let item = input.item_id;
         let quantity = input.quantity;
         let queryString = `SELECT * FROM products WHERE item_id = ${item}`;
 
 
-        connection.query(queryString), {item_id: item}, function(error, data){
-            if(error) throw error;
-            if(data.length === 0){
+        connection.query(queryString, function (error, data) {
+            if (error) throw error;
+            if (data.length === 0) {
                 console.log("Invalid Item ID");
                 displayInventory();
-            }else{
+            } else {
                 let productData = data[0];
-                if(quantity<= productData.stock_quantity){
+                if (quantity <= productData.stock_quantity) {
                     console.log("Your order was placed!");
-                    let updatedString =  `UPDATE products SET stock_quantity = ${(productData.stock_quantity - quantity)} WHERE item_id = ${item}`;
-                    connection.query(updatedString, function(error, data){
-                        if(error) throw error;
+                    let updatedString = `UPDATE products SET stock_quantity = ${(productData.stock_quantity - quantity)} WHERE item_id = ${item}`;
+                    connection.query(updatedString, function (error, data) {
+                        if (error) throw error;
                         console.log("Thanks! Your total is $" + productData.price * quantity);
                         connection.end();
                     })
-                }else{
+                } else {
                     console.log("Sorry, your order can't be placed. That item is currently out of stock");
                     displayInventory();
                 }
             }
-        }
-    })
+        })
+    });
 }
-
-function displayInventory(){
+function displayInventory() {
     queryString = 'SELECT * FROM products';
-    connection.query(queryString, function(error, data){
-        if(error) throw error;
+    connection.query(queryString, function (error, data) {
+        if (error) throw error;
         console.log("Current Product Inventory: ");
         console.log("__________________________");
 
         var strOut = '';
-		for (var i = 0; i < data.length; i++) {
-			strOut = '';
-			strOut += 'Item ID: ' + data[i].item_id + '  //  ';
-			strOut += 'Product Name: ' + data[i].product_name + '  //  ';
-			strOut += 'Department: ' + data[i].department_name + '  //  ';
-			strOut += 'Price: $' + data[i].price + '\n';
+        for (var i = 0; i < data.length; i++) {
+            strOut = '';
+            strOut += 'Item ID: ' + data[i].item_id + '  //  ';
+            strOut += 'Product Name: ' + data[i].product_name + '  //  ';
+            strOut += 'Department: ' + data[i].department_name + '  //  ';
+            strOut += 'Price: $' + data[i].price + '\n';
 
             console.log(strOut);
         }
@@ -80,8 +79,9 @@ function displayInventory(){
     })
 }
 
-function run(){
+function run() {
     displayInventory();
 }
 
 run();
+
